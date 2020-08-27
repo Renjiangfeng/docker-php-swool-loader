d9e1cf0692fdc7e40222ba82bbfa7e7ab399d4d5
Ryan_Ren <renrr@shinetechchina.com>
2020-08-27 18:07:56 +0800

    优化了语法

--- encry.sh
+++ encry.sh
@@ -1,14 +1,14 @@
-#!/bin/bash/
+#!/bin/bash
 #TODO Code encryption
 code_path="/home/ubuntu/encry/"
 web_path="/home/wwwroot/web/"
 log_path="/home/ubuntu/"
 mdate=$(date +%Y_%m_%d_%H_%M_%S)
 if [ ! -d $web_path ]; then
-  mkdir -p  $web_path
+  mkdir -p $web_path
   chmod 755 -R $web_path
   chown www:root $web_path
-   cd $code_path
+  cd $code_path
   tar -czf encry.tar.gz app
   tar -zxvf encry.tar.gz -C $web_path
   cd $log_path
--- install_swoole_loader.sh
+++ install_swoole_loader.sh
@@ -2,9 +2,11 @@
 #swoole_loader extension file
 swoole_extension_file='/home/ubuntu/swoole_loader.so'
 #查找 php extension dir （默认的php版本  ，若其他版本： php -i改为path/7.1/php -i）
-PHP_EXT_DIR=`php -i | grep "extension_dir" | awk ' /php/  { print $3"/"}'`
+#PHP_EXT_DIR=`php -i | grep "extension_dir" | awk ' /php/  { print $3"/"}'`
+PHP_EXT_DIR=$(php -i | grep "extension_dir" | awk ' /php/  { print $3"/"}')
 # 查找php.ini path（默认的php版本  ，若其他版本： php -i改为path/7.1/php -i）
-PHP_INI_PATH=`php -i | grep "php.ini"  | awk   -F ' => ' '$2 ~ /php.ini/  { print $2  }'`
+#PHP_INI_PATH=`php -i | grep "php.ini"  | awk   -F ' => ' '$2 ~ /php.ini/  { print $2  }'`
+PHP_INI_PATH=$(php -i | grep "php.ini"  | awk   -F ' => ' '$2 ~ /php.ini/  { print $2  }')
 
 
 
@@ -20,12 +22,13 @@ php_swool_loader() {
 }
 #load  swool_loader
 update_swoole_loader(){
-  if [ $(grep -w ";extension=swoole_loader.so" $PHP_INI_PATH) ];   then
-      sed -i '/;extension=swoole_loader.so/d'  $PHP_INI_PATH && echo "extension=swoole_loader.so" >> $PHP_INI_PATH
+  if grep -w ";extension=swoole_loader.so" "$PHP_INI_PATH"
+     then
+      sed -i '/;extension=swoole_loader.so/d'  "$PHP_INI_PATH" && echo "extension=swoole_loader.so" >> "$PHP_INI_PATH"
       echo "swoole_loader was installed successfully!"
     else
 #      echo 'no'
-      cp $swoole_extension_file $PHP_EXT_DIR && echo "extension=swoole_loader.so" >> $PHP_INI_PATH
+      cp $swoole_extension_file "$PHP_EXT_DIR" && echo "extension=swoole_loader.so" >> "$PHP_INI_PATH"
       echo "swoole_loader was installed successfully!"
   fi
 }
